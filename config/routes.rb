f8fa829ed5e8d5174b4eb44d19fe8e314adc0011
takahiro-shimizu <tkbchando@gmail.com>
2021-05-01 09:00:32 +0000

    [Add]ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  
-  devise_for :views
   devise_for :users
+  
+  resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
+  resources :users, only: [:edit, :show, :update]
 end
