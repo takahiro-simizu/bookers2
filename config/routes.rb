996a58d62749731dd8ce799106986671745e7bdc
takahiro-shimizu <tkbchando@gmail.com>
2021-05-02 13:00:29 +0000

    [Add]ヘッダー、TOPページ、Aboutページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   
+  get "home/about" => 'homes#about'
+  
   resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
-  resources :users, only: [:edit, :show, :update]
+  resources :users, only: [:edit, :show, :update, :index]
 end
